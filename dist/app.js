d2f2a7293f34db4b6c24a4ec406fb6c8a73a217c
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-06-24 19:12:58 +0200

    Update JS build file

--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,50 @@
-"use strict";
+'use strict';
 const processClick = (e) => {
-    if (e.target.nodeName === 'IMG' && e.target.id === 'open') {
-        document.querySelector('#menuDiv').classList.toggle('hide');
-        document.querySelector('#menuDiv').classList.toggle('show');
-        e.target.classList.toggle('hide');
-        document.querySelector('#close').classList.toggle('hide');
-    }
-    else if (e.target.nodeName === 'IMG' && e.target.id === 'close') {
-        document.querySelector('#menuDiv').classList.toggle('show');
-        document.querySelector('#menuDiv').classList.toggle('hide');
-        e.target.classList.toggle('hide');
-        document.querySelector('#open').classList.toggle('hide');
-    }
-    else if (e.target.nodeName === 'LI' && e.target.classList.contains('listItem')) {
-        e.target.classList.toggle('clicked');
-        e.target.querySelectorAll('.arrow').forEach((arrow) => { arrow.classList.toggle('flip'); });
-        e.target.querySelector('.dropDown').classList.toggle('hide');
-    }
+  if (e.target.nodeName === 'IMG' && e.target.id === 'open') {
+    document.querySelector('#menuDiv').classList.toggle('hide');
+    document.querySelector('#menuDiv').classList.toggle('show');
+    e.target.classList.toggle('hide');
+    document.querySelector('#close').classList.toggle('hide');
+  } else if (e.target.nodeName === 'IMG' && e.target.id === 'close') {
+    document.querySelector('#menuDiv').classList.toggle('show');
+    document.querySelector('#menuDiv').classList.toggle('hide');
+    e.target.classList.toggle('hide');
+    document.querySelector('#open').classList.toggle('hide');
+  } else if (
+    e.target.nodeName === 'LI' &&
+    e.target.classList.contains('listItem')
+  ) {
+    document.querySelectorAll('.listItem').forEach((item) => {
+      if (item.classList.contains('clicked') && item !== e.target) {
+        item.querySelectorAll('.arrow').forEach((arrow) => {
+          arrow.classList.toggle('flip');
+        });
+        item.querySelector('.dropDown').classList.toggle('hide');
+        item.classList.toggle('clicked');
+      }
+    });
+    e.target.classList.toggle('clicked');
+    e.target.querySelectorAll('.arrow').forEach((arrow) => {
+      arrow.classList.toggle('flip');
+    });
+    e.target.querySelector('.dropDown').classList.toggle('hide');
+  } else {
+    document.querySelectorAll('.dropDown').forEach((item) => {
+      if (!item.classList.contains('hide')) {
+        document.querySelectorAll('.listItem').forEach((listItem) => {
+          if (listItem.classList.contains('clicked')) {
+            listItem.querySelectorAll('.arrow').forEach((arrow) => {
+              arrow.classList.toggle('flip');
+            });
+            listItem.classList.toggle('clicked');
+          }
+        });
+        item.classList.toggle('hide');
+      }
+    });
+  }
 };
 const init = () => {
-    document.addEventListener('click', processClick);
+  document.addEventListener('click', processClick);
 };
 document.addEventListener('DOMContentLoaded', init);
